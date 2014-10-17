788a5238f1839662a7437aacd1c78a92b8f74829
Justin Edelson <justin@justinedelson.com>
2014-10-16 21:05:04 -0400

    fixing issue where css.txt and js.txt were missing. fixes #3

--- templates/aem-multimodule-project/lazybones.groovy
+++ templates/aem-multimodule-project/lazybones.groovy
@@ -383,6 +383,8 @@ This client library should be used to embed dependencies. It is pre-stocked with
 common AEM dependencies, but you should modify to meet your needs. In general, this will need
 to be loaded in the head of your page in order to reduce extra HTTP calls.
 """)
+        writeToFile(depClientLibFolder, "css.txt", "")
+        writeToFile(depClientLibFolder, "js.txt", "")
     }
 }
 
