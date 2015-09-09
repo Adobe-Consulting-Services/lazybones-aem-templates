Adobe-Consulting-Services/lazybones-aem-templates

commit 316054dae884785305003c65b226659c788e3f92

updating ACS AEM Commons version

templates/aem-multimodule-project/lazybones.groovy
@@ -106,7 +106,8 @@ props.bundleDependencies.addAll([osgiCore, osgiCompendium, scrAnnotations, bndAn
 props.contentDependencies.addAll([osgiCore, osgiCompendium, servletApi, commonsLang3, commonsLang2, jstl, jsp, jcr])
 
 // Constants
-def ACS_AEM_COMMONS_VERSION = "1.10.2"
+def ACS_AEM_COMMONS_VERSION_5 = "1.10.4"
+def ACS_AEM_COMMONS_VERSION_6 = "2.0.0"
 def AEM60_API_VERSION = "6.0.0.1"
 def AEM61_API_VERSION = "6.1.0"
 
@@ -207,8 +208,11 @@ if (props.aemVersion == VERSION_60) {
 // ACS AEM Commons
 props.includeAcsAemCommons = askBoolean("Include ACS AEM Commons as a dependency? [yes]: ", "yes", "includeAcsAemCommons")
 if (props.includeAcsAemCommons) {
-    def bundle = dependency("com.adobe.acs", "acs-aem-commons-bundle", ACS_AEM_COMMONS_VERSION)
-
+    if (props.aemVersion == VERSION_561) {
+        def bundle = dependency("com.adobe.acs", "acs-aem-commons-bundle", ACS_AEM_COMMONS_VERSION_5)
+    } else {
+        def bundle = dependency("com.adobe.acs", "acs-aem-commons-bundle", ACS_AEM_COMMONS_VERSION_6)
+    }
     props.rootDependencies.add(bundle)
     props.bundleDependencies.add(bundle)
     props.contentDependencies.add(bundle)

templates/aem-multimodule-project/VERSION
@@ -1,1 +1,1 @@
-0.0.20-SNAPSHOT
+0.0.20

commit 0f7899134a9de34c60b684f65ce2328caa3598e5

fixing error introduced in 316054dae884785305003c65b226659c788e3f92

templates/aem-multimodule-project/lazybones.groovy
@@ -208,10 +208,11 @@ if (props.aemVersion == VERSION_60) {
 // ACS AEM Commons
 props.includeAcsAemCommons = askBoolean("Include ACS AEM Commons as a dependency? [yes]: ", "yes", "includeAcsAemCommons")
 if (props.includeAcsAemCommons) {
+    def bundle;
     if (props.aemVersion == VERSION_561) {
-        def bundle = dependency("com.adobe.acs", "acs-aem-commons-bundle", ACS_AEM_COMMONS_VERSION_5)
+        bundle = dependency("com.adobe.acs", "acs-aem-commons-bundle", ACS_AEM_COMMONS_VERSION_5)
     } else {
-        def bundle = dependency("com.adobe.acs", "acs-aem-commons-bundle", ACS_AEM_COMMONS_VERSION_6)
+        bundle = dependency("com.adobe.acs", "acs-aem-commons-bundle", ACS_AEM_COMMONS_VERSION_6)
     }
     props.rootDependencies.add(bundle)
     props.bundleDependencies.add(bundle)
@@ -220,7 +221,12 @@ if (props.includeAcsAemCommons) {
     props.includeAcsAemCommonsSubPackage = askBoolean("Include ACS AEM Commons as a sub-package? [yes]: ", "yes", "includeAcsAemCommonsSubPackage")
 
     if (props.includeAcsAemCommonsSubPackage) {
-        def content = dependency("com.adobe.acs", "acs-aem-commons-content", ACS_AEM_COMMONS_VERSION, "content-package")
+        def content;
+        if (props.aemVersion == VERSION_561) {
+            content = dependency("com.adobe.acs", "acs-aem-commons-content", ACS_AEM_COMMONS_VERSION_5, "content-package")
+        } else {
+            content = dependency("com.adobe.acs", "acs-aem-commons-content", ACS_AEM_COMMONS_VERSION_6, "content-package")
+        }
         props.rootDependencies.add(content)
         props.contentDependencies.add(content)
     }